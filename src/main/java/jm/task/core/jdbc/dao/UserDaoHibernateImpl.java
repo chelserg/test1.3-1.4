401847c2a5aff12a8d95ed2bbc05ba9d20b73709
Sergey Chelyshev <sergey36russia@gmail.com>
2024-11-03 15:31:36 +0300

    Initial commit

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,42 +1,82 @@
 package jm.task.core.jdbc.dao;
 
+
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 
 import java.util.List;
 
+
 public class UserDaoHibernateImpl implements UserDao {
+
+
+    private static final SessionFactory sessionFactory = Util.getSessionFactory();
+
     public UserDaoHibernateImpl() {
 
     }
 
-
     @Override
     public void createUsersTable() {
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        session.createNativeQuery("CREATE TABLE IF NOT EXISTS users " +
+                "(id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY, " +
+                "name VARCHAR(50) NOT NULL, lastName VARCHAR(50) NOT NULL, " +
+                "age TINYINT NOT NULL)" ).executeUpdate();
+        session.getTransaction().commit();
 
     }
 
     @Override
     public void dropUsersTable() {
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        session.createNativeQuery("DROP TABLE IF EXISTS users").executeUpdate();
+        session.getTransaction().commit();
+
 
     }
-
     @Override
     public void saveUser(String name, String lastName, byte age) {
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        User user = new User();
+        user.setName(name);
+        user.setLastName(lastName);
+        user.setAge(age);
+        session.save(user);
+        session.getTransaction().commit();
 
     }
 
     @Override
     public void removeUserById(long id) {
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        User user = (User) session.get(User.class, id);
+        session.delete(user);
+        session.getTransaction().commit();
 
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        List<User> users = session.createQuery("from User").list();
+        session.getTransaction().commit();
+        return users;
+
     }
 
     @Override
     public void cleanUsersTable() {
-
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        session.createNativeQuery("DELETE FROM users").executeUpdate();
+        session.getTransaction().commit();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,15 +1,25 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Util {
 
     private static final String DB_URL = "jdbc:mysql://localhost:3306/test_db";
     private static final String USER = "root";
     private static final String PASSWORD = "root";
-
+    private static final String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String DIALECT = "org.hibernate.dialect.MySQL5Dialect";
+    private static SessionFactory sessionFactory;
 
 
     public static Connection getConnection() {
@@ -23,5 +33,30 @@ public class Util {
 
 
 }
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                Configuration config = new Configuration();
+                Properties props = new Properties();
+                props.put(Environment.DRIVER, DB_DRIVER);
+                props.put(Environment.URL, DB_URL);
+                props.put(Environment.USER, USER);
+                props.put(Environment.PASS, PASSWORD);
+                props.put(Environment.DIALECT, DIALECT);
+                config.setProperties(props);
+
+                config.addAnnotatedClass(User.class);
+                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                        .applySettings(config.getProperties()).build();
+                sessionFactory = config.buildSessionFactory(serviceRegistry);
+            } catch (Exception e) {
+                System.out.println("Problem creating session factory");
+                e.printStackTrace();
+            }
+        }
+        return sessionFactory;
+    }
+
 
     }
